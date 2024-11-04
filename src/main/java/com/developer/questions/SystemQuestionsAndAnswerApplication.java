e773dda2b30f869e20318395ec31cd5576a7ae08
ednaleiden <ednadupont@hotmail.com>
2024-11-04 17:32:58 -0500

    questions

---
+++ src/main/java/com/developer/questions/SystemQuestionsAndAnswerApplication.java
@@ -0,0 +1,13 @@
+package com.developer.questions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemQuestionsAndAnswerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemQuestionsAndAnswerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/developer/questions/SystemQuestionsAndAnswerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer.questions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemQuestionsAndAnswerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
