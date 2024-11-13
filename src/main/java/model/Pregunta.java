f7e9f085711aa3f9353ab9c1a3056661dee44dae
ednaleiden <ednadupont@hotmail.com>
2024-11-13 17:59:08 -0500

    implements model

--- src/main/java/model/Pregunta.java
+++ src/main/java/model/Pregunta.java
@@ -1,4 +1,30 @@
 package model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Pregunta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String contenido;
+
+    @ManyToOne
+    @JoinColumn(name = "encuesta_id")
+    private Encuesta encuesta;
+
+
+    @OneToMany(mappedBy = "pregunta", cascade = CascadeType.ALL)
+    private List<Respuesta> respuestas = new ArrayList<>();
 }
--- src/main/java/model/Respuesta.java
+++ src/main/java/model/Respuesta.java
@@ -1,4 +1,24 @@
 package model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Respuesta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String contenido;
+
+    @ManyToOne
+    @JoinColumn(name = "pregunta_id")
+    private Pregunta pregunta;
+
 }
